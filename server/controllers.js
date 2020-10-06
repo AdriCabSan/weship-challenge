72ed51127d00332eef56bfab33d2cb1d7c82116c
AdrianCabrera <adriancabsan98@gmail.com>
2020-10-06 17:58:14 -0500

    Feature[Frontend]:Add Components

--- server/controllers.js
+++ server/controllers.js
@@ -2,14 +2,14 @@
 const axios = require('axios');
 const { response } = require('express');
 const convertXmlToJson = require('xml-js')
-
+const domain="http://3BATE125JJIX4M3I2NJSTVFCS9TS6GD9@18.219.54.186"
 
 module.exports = {
     getOrders:
     async function getOrders(response) {
         try {
           const { data } = await axios.get(
-            "http://3BATE125JJIX4M3I2NJSTVFCS9TS6GD9@18.219.54.186/api/orders?display=full"
+            `${domain}/api/orders?display=full`
           );
           orders = (convertXmlToJson.xml2js(data,{compact: true, spaces: 2}))
           response.json(orders.prestashop.orders)
@@ -21,12 +21,24 @@ module.exports = {
       async function getCarriers(response) {
           try {
             const { data } = await axios.get(
-              "http://3BATE125JJIX4M3I2NJSTVFCS9TS6GD9@18.219.54.186/api/carriers?display=full"
+              `${domain}/api/carriers?display=full`
             );
             carriers = (convertXmlToJson.xml2js(data,{compact: true, spaces: 2}))
             response.json(carriers.prestashop.carriers)
           } catch (error) {
             console.log(error);
           }
+        },
+      getOderCarriers: 
+      async function getOderCarriers(response) {
+        try {
+          let { data } = await axios.get(
+            `${domain}/api/order_carriers?display=[id_order,id_carrier]`
+          );
+          order_carriers = (convertXmlToJson.xml2js(data,{compact: true, spaces: 2}))
+          response.json(order_carriers.prestashop.carriers)
+        } catch (error) {
+          console.log(error);
         }
+      }
 }
--- src/components/order/order-table.js
+++ src/components/order/order-table.js
@@ -59,17 +59,16 @@ const columns = [
   }
 ];
 
-function createData(name, calories, fat, carbs, protein, price) {
+function createData(id, id_address_delivery, invoice_number, shipping_number, payment) {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      { date: "2020-01-05", customerId: "11091700", amount: 3 },
-      { date: "2020-01-02", customerId: "Anonymous", amount: 1 }
+    id,
+    id_address_delivery,
+    invoice_number,
+    shipping_number,
+    payment,
+    product: [
+      { id: "2020-01-05", productId: "2", productName: "Hummingbird printed t-shirt - Color : White, Size : S" },
+      { id: "2020-01-05", productId: "3", productName: "Hummingbird printed t-shirt - Color : White, Size : S" },
     ]
   };
 }
@@ -92,19 +91,19 @@ function Row(props) {
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-          {row.name}
+          {row.id}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
+        <TableCell align="right">{row.id_address_delivery}</TableCell>
+        <TableCell align="right">{row.invoice_number}</TableCell>
+        <TableCell align="right">{row.shipping_number}</TableCell>
+        <TableCell align="right">{row.payment}</TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box margin={1}>
               <Typography variant="h6" gutterBottom component="div">
-                History
+                Product
               </Typography>
               <Table size="small" aria-label="purchases">
                 <TableHead>
@@ -116,16 +115,14 @@ function Row(props) {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
+                  {row.product.map((productRow) => (
+                    <TableRow key={productRow.date}>
                       <TableCell component="th" scope="row">
-                        {historyRow.date}
+                        {productRow.date}
                       </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
+                      <TableCell>{productRow.id}</TableCell>
+                      <TableCell align="right">{productRow.productId}</TableCell>
+                      <TableCell align="right">{productRow.productName}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -143,7 +140,7 @@ Row.propTypes = {
     calories: PropTypes.number.isRequired,
     carbs: PropTypes.number.isRequired,
     fat: PropTypes.number.isRequired,
-    history: PropTypes.arrayOf(
+    product: PropTypes.arrayOf(
       PropTypes.shape({
         amount: PropTypes.number.isRequired,
         customerId: PropTypes.string.isRequired,
